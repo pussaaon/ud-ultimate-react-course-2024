503134a9fa2c9bdc39a9832fdb2a46358714d358
Lauv <pussaaon@gmail.com>
2024-01-01 17:02:12 +0700

    Project Worldwise: Integrate geolocation

--- hand-ons/part3/worldwise/src/components/AddForm.jsx
+++ hand-ons/part3/worldwise/src/components/AddForm.jsx
@@ -1,10 +1,13 @@
-// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
+// 
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./AddForm.module.css";
 import Button from "./Button";
 import BackButton from "./BackButton";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+import Spinner from "./Spinner";
+import Message from "./Message";
 
 export function convertToEmoji(countryCode) {
     const codePoints = countryCode
@@ -20,6 +23,40 @@ function Form() {
     const [country, setCountry] = useState("");
     const [date, setDate] = useState(new Date());
     const [notes, setNotes] = useState("");
+
+    const [lat, lng] = useUrlPosition();
+    const [isLoadingGeoData, setIsLoadingGeoData] = useState(false);
+    const [geoError, setGeoError] = useState(null);
+    const [emoji, setEmoji] = useState(null);
+    const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
+    useEffect(() => {
+        async function fetchCityData() {
+            try {
+                setIsLoadingGeoData(true);
+                setGeoError(null);
+                const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+                const data = await res.json();
+                console.log(data);
+
+                if (!data.countryCode) {
+                    throw new Error("That's doesn't seem to be a city, Click somewhere else");
+                }
+
+                setCityName(data.city || data.locality || "");
+                setCountry(data.countryName);
+                setEmoji(convertToEmoji(data.countryCode));
+            } catch (err) {
+                setGeoError(err.message);
+            } finally {
+                setIsLoadingGeoData(false);
+            }
+        }
+        fetchCityData();
+    }, [lat, lng]);
+
+    if (isLoadingGeoData) return <Spinner />
+    if (geoError) return <Message message={geoError} />
 
     return (
         <form className={styles.form}>
--- hand-ons/part3/worldwise/src/components/Map.jsx
+++ hand-ons/part3/worldwise/src/components/Map.jsx
@@ -1,16 +1,22 @@
 import styles from './Map.module.css'
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet'
 import { useEffect, useState } from 'react';
 import { useCitiesContext } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation.js';
+import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition.js';
 
 function Map() {
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
     const { cities } = useCitiesContext();
+    const {
+        isLoading: isLoadingPosition,
+        position: geoLocation,
+        getPosition,
+    } = useGeolocation();
 
     const [mapPosition, setMapPosition] = useState([40, 0]);
+    const [lat, lng] = useUrlPosition();
 
     useEffect(() => {
         if (lat && lng) {
@@ -18,8 +24,17 @@ function Map() {
         }
     }, [lat, lng]);
 
+    useEffect(() => {
+        if (geoLocation) {
+            setMapPosition([geoLocation.lat, geoLocation.lng]);
+        }
+    }, [geoLocation]);
+
     return (
         <div className={styles.mapContainer}>
+            {!geoLocation && <Button className="position" onClick={getPosition}>
+                {isLoadingPosition ? "Loading..." : "Use current position"}
+            </Button>}
             <MapContainer
                 center={mapPosition}
                 zoom={13}
