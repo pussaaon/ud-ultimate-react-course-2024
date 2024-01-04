361cf5435e98f173fba2a45219bb4f373b97c385
Lauv <pussaaon@gmail.com>
2024-01-04 16:05:24 +0700

    Project Redux Intro: Integrate React with Redux store

--- hand-ons/part3/redux-intro/src/index.js
+++ hand-ons/part3/redux-intro/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from "react-redux";
 import './index.css';
 import App from './App';
 import store from './store';
 
 store.dispatch({ type: "account/deposit", payload: 250 });
+store.dispatch({ type: "customer/createCustomer", payload: { fullName: "Lauv", nationalID: "2323" } });
 console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
--- hand-ons/part3/redux-intro/src/store.js
+++ hand-ons/part3/redux-intro/src/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, createStore } from "redux";
-import accountReducer from "./accountSlice";
-import customerReducer from "./customerSlice";
+import accountReducer from "./features/accounts/accountSlice.js";
+import customerReducer from "./features/customers/customerSlice.js";
 
 const rootReducer = combineReducers({
     account: accountReducer,
