1c7144aca1b177241fcc950d2037ba4f82dda21f
Lauv <pussaaon@gmail.com>
2024-01-06 09:33:43 +0700

    Project Redux Intro: Apply middleware

--- hand-ons/part3/redux-intro/src/features/accounts/accountSlice.js
+++ hand-ons/part3/redux-intro/src/features/accounts/accountSlice.js
@@ -7,7 +7,7 @@ const initialState = {
 function accountReducer(state = initialState, action) {
     switch (action.type) {
         case "account/deposit":
-            return { ...state, balance: state.balance + action.payload };
+            return { ...state, balance: state.balance + action.payload, isLoading: false };
         case "account/withdraw":
             return { ...state, balance: state.balance - action.payload };
         case "account/requestLoan":
@@ -15,13 +15,27 @@ function accountReducer(state = initialState, action) {
             return { ...state, loan: action.payload.amount, loanPurpose: action.payload.purpose };
         case "account/payLoan":
             return { ...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan }
+        case "account/convertingCurrency":
+            return { ...state, isLoading: true };
         default:
             return state;
     }
 }
 
-function deposit(amount) {
-    return { type: 'account/deposit', payload: amount }
+function deposit(amount, currency) {
+    if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+    return async function (dispatch, getState) {
+        dispatch({ type: "account/convertingCurrency" });
+
+        const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`);
+
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        dispatch({ type: 'account/deposit', payload: converted });
+    }
 }
 
 function withdraw(amount) {
