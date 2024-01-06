03e4209c67661f71b49ad98a718e6a2c87305c32
Lauv <pussaaon@gmail.com>
2024-01-06 20:33:56 +0700

    Project WorldWiser: Handle case no city

--- hand-ons/part3/worldwiser/src/components/CityList.jsx
+++ hand-ons/part3/worldwiser/src/components/CityList.jsx
@@ -1,10 +1,11 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
-    console.log(cities);
     if (isLoading) return <Spinner />;
+    if (!cities.length) return <Message message="Start adding a city from the map." />;
     return <ul className={styles.cityList}>
         {cities.map(city => <CityItem key={city.id} city={city} />)}
     </ul>
