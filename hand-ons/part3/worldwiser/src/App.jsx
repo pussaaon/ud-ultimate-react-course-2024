82b7b0ab3f3f9bce2cd11c33eaa1d56b69051c9d
Lauv <pussaaon@gmail.com>
2024-01-07 10:27:23 +0700

    Project WorldWiser: Add param and queries between pages for lat, lng and city id

--- hand-ons/part3/worldwiser/src/App.jsx
+++ hand-ons/part3/worldwiser/src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
+import City from "./components/City"
 import { useState, useEffect } from "react"
 import "./index.css"
 
@@ -44,6 +45,7 @@ function App() {
                 <Route path="/app" element={<AppLayout />}>
                     <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
                     <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+                    <Route path="cities/:id" element={<City />} />
                     <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
                     <Route path="form" element={<p>Form</p>} />
                 </Route>
--- hand-ons/part3/worldwiser/src/components/Map.jsx
+++ hand-ons/part3/worldwiser/src/components/Map.jsx
@@ -1,9 +1,19 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get("lat");
+    const lng = searchParams.get("lng");
+
+
     return (
         <div className={styles.mapContainer}>
             <p>Map</p>
+            <p>Position: {lat}, {lng}</p>
+
+            <button onClick={() => setSearchParams({ lat: 22, lng: 33 })}>Change Pos</button>
         </div>
     )
 }
