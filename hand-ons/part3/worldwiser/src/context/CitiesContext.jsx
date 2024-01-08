f7f2c82d454699877dc4d53a1dbbd25dc774f780
Lauv <pussaaon@gmail.com>
2024-01-08 09:49:36 +0700

    Project WorldWiser: Handle delete city

--- hand-ons/part3/worldwiser/src/context/CitiesContext.jsx
+++ hand-ons/part3/worldwiser/src/context/CitiesContext.jsx
@@ -59,6 +59,20 @@ function CitiesProvider({ children }) {
         }
     }
 
+    async function deleteCity(id) {
+        try {
+            setIsLoading(true);
+            await fetch(`${BASE_URL}/cities/${id}`, {
+                method: "DELETE"
+            })
+            setCities((cities) => cities.filter((city) => city.id !== id));
+        } catch (err) {
+            console.log(`Error deleting city: ${id} `, err.message)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <CitiesContext.Provider
             value={{
@@ -66,6 +80,7 @@ function CitiesProvider({ children }) {
                 isLoading,
                 getCity,
                 addCity,
+                deleteCity,
                 currentCity
             }}>
             {children}
