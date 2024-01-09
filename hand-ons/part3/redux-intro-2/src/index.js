e2dfb5abe496a2fafae54896ad945ea15d7b6caa
Lauv <pussaaon@gmail.com>
2024-01-09 20:19:42 +0700

    Project Redux Intro 2: Connect app to redux store with Provider and useSelector

--- hand-ons/part3/redux-intro-2/src/index.js
+++ hand-ons/part3/redux-intro-2/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-
 import store from "./store";
+
+import './index.css';
 
 store.dispatch({ type: "account/deposit", payload: 1000 });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
