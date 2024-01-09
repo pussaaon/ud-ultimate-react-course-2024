e851155dadacba0ba39e08452219140fecb41197
Lauv <pussaaon@gmail.com>
2024-01-09 20:50:25 +0700

    Product Redux Intro 2: Handle create customer and account operation with useDispatch and useSelector

--- hand-ons/part3/redux-intro-2/src/App.js
+++ hand-ons/part3/redux-intro-2/src/App.js
@@ -2,16 +2,26 @@ import CreateCustomer from "./features/customer/CreateCustomer";
 import Customer from "./features/customer/Customer";
 import AccountOperations from "./features/account/AccountOperations";
 import BalanceDisplay from "./features/account/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+
+    const customer = useSelector(store => store.customer);
+
     return (
         <div>
             <h1>🏦 The React-Redux Bank ⚛️</h1>
-            <CreateCustomer />
-            <Customer />
-            <AccountOperations />
-            <BalanceDisplay />
-        </div>
+            {customer.fullName === ""
+                ? (<CreateCustomer />)
+                : (
+                    <>
+                        <Customer />
+                        <AccountOperations />
+                        <BalanceDisplay />
+                    </>
+                )
+            }
+        </div >
     );
 }
 
--- hand-ons/part3/redux-intro-2/src/features/account/AccountOperations.js
+++ hand-ons/part3/redux-intro-2/src/features/account/AccountOperations.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw, requestLoan, payLoan } from "./accountSlice";
 
 function AccountOperations() {
     const [depositAmount, setDepositAmount] = useState("");
@@ -7,13 +9,36 @@ function AccountOperations() {
     const [loanPurpose, setLoanPurpose] = useState("");
     const [currency, setCurrency] = useState("USD");
 
-    function handleDeposit() { }
+    const dispatch = useDispatch();
+    const {
+        loan: currentLoan,
+        loanPurpose: currentLoanPurpose,
+        balance
+    } = useSelector((store) => store.account);
 
-    function handleWithdrawal() { }
+    function handleDeposit() {
+        if (!depositAmount) return;
+        dispatch(deposit(depositAmount));
+        setDepositAmount("");
+    }
 
-    function handleRequestLoan() { }
+    function handleWithdrawal() {
+        if (!withdrawalAmount) return;
+        dispatch(withdraw(withdrawalAmount));
+        setWithdrawalAmount("");
+    }
 
-    function handlePayLoan() { }
+    function handleRequestLoan() {
+        if (!loanAmount || !loanPurpose) return;
+        dispatch(requestLoan(loanAmount, loanPurpose));
+        setLoanAmount("");
+        setLoanPurpose("");
+    }
+
+    function handlePayLoan() {
+        if (!currentLoan) return;
+        dispatch(payLoan());
+    }
 
     return (
         <div>
@@ -65,11 +90,12 @@ function AccountOperations() {
                     />
                     <button onClick={handleRequestLoan}>Request loan</button>
                 </div>
-
-                <div>
-                    <span>Pay back $X</span>
-                    <button onClick={handlePayLoan}>Pay loan</button>
-                </div>
+                {currentLoan &&
+                    <div>
+                        <span>Pay back $X</span>
+                        <button onClick={handlePayLoan}>Pay loan</button>
+                    </div>
+                }
             </div>
         </div>
     );
