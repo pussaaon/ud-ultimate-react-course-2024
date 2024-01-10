608608ff07f6a546eda26592b3b91e84e66402bb
Lauv <pussaaon@gmail.com>
2024-01-10 15:56:33 +0700

    project fast-react-pizza: handle create new order form

--- hand-ons/part4/fast-react-pizza/src/App.jsx
+++ hand-ons/part4/fast-react-pizza/src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, { action as createOrderAction } from './features/order/CreateOrder';
 import Order, { loader as orderLoader } from './features/order/Order';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
@@ -19,8 +19,12 @@ const router = createBrowserRouter([
                 loader: menuLoader,
                 errorElement: <Error />
             },
-            { path: '/cart', element: <Cart /> },
-            { path: '/order/new', element: <CreateOrder /> },
+            { path: '/cart', element: <Cart />, },
+            {
+                path: '/order/new',
+                element: <CreateOrder />,
+                action: createOrderAction,
+            },
             {
                 path: '/order/:orderId',
                 element: <Order />,
