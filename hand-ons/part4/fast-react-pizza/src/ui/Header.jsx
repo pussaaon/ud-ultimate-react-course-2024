9d69ec6ebd005ddcad19241969cd3873366ad3cb
Lauv <pussaaon@gmail.com>
2024-01-10 14:27:39 +0700

    project fast-react-pizza: add AppLayout route over pages

---
+++ hand-ons/part4/fast-react-pizza/src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <>
+            <header><Link to="/">Fast React Pizza Co.</Link></header>
+            <p>Open</p>
+        </>
+    )
+}
+
+export default Header
