1ad5e8cb5d940bdecd8e459320b50e907bdbf1b0
Lauv <pussaaon@gmail.com>
2024-01-10 07:56:15 +0700

    Project Redux Intro: Apply configureStore from redux toolkit

--- hand-ons/part3/redux-intro-2/src/store.js
+++ hand-ons/part3/redux-intro-2/src/store.js
@@ -1,12 +1,12 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const rootReducer = combineReducers({
-    account: accountReducer,
-    customer: customerReducer
+const store = configureStore({
+    reducer: {
+        account: accountReducer,
+        customer: customerReducer
+    }
 });
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
 export default store;
--- part3-sec20-redux-rtx-thunks.md
+++ part3-sec20-redux-rtx-thunks.md
@@ -5,6 +5,7 @@ npm i redux
 npm i react-redux
 npm i redux-thunk
 npm i redux-devtools-extension
+npm i @reduxjs/toolkit
 ```
 
 # What is Redux?
