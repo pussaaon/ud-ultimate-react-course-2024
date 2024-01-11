611b578b548b2860eb3be95c4812ea71920f2949
Lauv <pussaaon@gmail.com>
2024-01-11 12:50:27 +0700

    project fast-react-pizza: reusing styles with react components.

--- hand-ons/part4/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ hand-ons/part4/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
+import Button from "../../ui/Button"
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -79,16 +80,9 @@ function CreateOrder() {
 
                 <div>
                     <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-                    <button disabled={isSubmitting}
-                        className="bg-yellow-400 uppercase font-semibold 
-                        text-stone-800 py-3 px-4 inline-block 
-                        tracking-wide ro
-                        unded-full 
-                        hover:bg-yellow-300 transition-colors duration-300 
-                        focus:outline-none focus:ring focus:ring-yellow-300 
-                        focus:bg-yellow-300 focus:ring-offset-2 
-                        disabled:cursor-not-allowed">
-                        {isSubmitting ? "Placing order..." : "Order now"}</button>
+                    <Button disabled={isSubmitting}>
+                        {isSubmitting ? "Placing order..." : "Order now"}
+                    </Button>
                 </div>
                 <div></div>
             </Form>
--- hand-ons/part4/fast-react-pizza/src/features/user/CreateUser.jsx
+++ hand-ons/part4/fast-react-pizza/src/features/user/CreateUser.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Button from "../../ui/Button";
 
 function CreateUser() {
     const [username, setUsername] = useState('');
@@ -15,14 +16,12 @@ function CreateUser() {
                 type="text"
                 placeholder="Your full name"
                 value={username}
-                className="w-72"
+                className="input w-72"
                 onChange={(e) => setUsername(e.target.value)}
             />
 
             {username !== '' && (
-                <div>
-                    <button>Start ordering</button>
-                </div>
+                <div><Button>Start ordering</Button></div>
             )}
         </form>
     );
