407a28246552f6e86dd493765d9cc9e8c4c3a102
Lauv <pussaaon@gmail.com>
2024-01-12 10:21:37 +0700

    project fast-react-pizza: handle cart page

--- hand-ons/part4/fast-react-pizza/src/features/cart/Cart.jsx
+++ hand-ons/part4/fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,35 +1,20 @@
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
-
-const fakeCart = [
-    {
-        pizzaId: 12,
-        name: 'Mediterranean',
-        quantity: 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    },
-    {
-        pizzaId: 6,
-        name: 'Vegetale',
-        quantity: 1,
-        unitPrice: 13,
-        totalPrice: 13,
-    },
-    {
-        pizzaId: 11,
-        name: 'Spinach and Mushroom',
-        quantity: 1,
-        unitPrice: 15,
-        totalPrice: 15,
-    },
-];
+import EmptyCart from './EmptyCart';
+import { useSelector, useDispatch } from 'react-redux';
+import { clearCart } from "./cartSlice";
 
 function Cart() {
-    const cart = fakeCart;
-    const username = useSelector(store => store.user.username);
+    const cart = useSelector(state => state.cart.cart);
+    const username = useSelector(state => state.user.username);
+    const dispatch = useDispatch();
+
+    if (cart.length === 0) return <EmptyCart />;
+
+    function handleClearCart() {
+        dispatch(clearCart());
+    }
 
     return (
         <div className="px-4 py-3">
@@ -38,13 +23,13 @@ function Cart() {
 
             <ul className="divide-y divide-stone-200 border-b mt-3">
                 {cart.map((item) => (
-                    <CartItem item={item} key={item.key} />
+                    <CartItem item={item} key={item.id} />
                 ))}
             </ul>
 
             <div className="mt-6 space-x-2">
                 <Button to="/order/new" type="primary">Order pizzas</Button>
-                <Button type="secondary">Clear cart</Button>
+                <Button type="secondary" onClick={handleClearCart}>Clear cart</Button>
             </div>
         </div>
     );
