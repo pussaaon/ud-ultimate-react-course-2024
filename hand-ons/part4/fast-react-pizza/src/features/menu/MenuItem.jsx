c724a98692a86891c99bb915030aefa32517c0bb
Lauv <pussaaon@gmail.com>
2024-01-12 09:27:48 +0700

    project fast-react-pizza: handle add item to cart

--- hand-ons/part4/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ hand-ons/part4/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,8 +1,22 @@
 import { formatCurrency } from "../../utils/helpers";
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
 import Button from "../../ui/Button";
 
 function MenuItem({ item }) {
     const { id, name, unitPrice, ingredients, soldOut, imageUrl } = item;
+    const dispatch = useDispatch();
+
+    function handleAddToCart() {
+        const newItem = {
+            pizzaId: id,
+            name: name,
+            quantity: 1,
+            unitPrice: unitPrice,
+            totalPrice: unitPrice
+        }
+        dispatch(addItem(newItem));
+    }
 
     return (
         <li className="flex gap-4 py-2">
@@ -14,9 +28,13 @@ function MenuItem({ item }) {
                 <p className="text-sm capitcalize italic text-stone-500">{ingredients.join(', ')}</p>
                 <div className="mt-auto flex items-center justify-between">
                     {!soldOut
-                        ? <p className="text-sm">{formatCurrency(unitPrice)}</p>
+                        ? <>
+                            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+                            <Button type="small"
+                                onClick={handleAddToCart}
+                            >Add to cart</Button>
+                        </>
                         : <p className="text-sm font-medium uppercase text-stone-500">Sold out</p>}
-                    <Button type="small">Add to cart</Button>
                 </div>
             </div>
         </li>
--- hand-ons/part4/fast-react-pizza/src/ui/Button.jsx
+++ hand-ons/part4/fast-react-pizza/src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
 
     const base = "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed text-sm";
 
@@ -16,6 +16,7 @@ function Button({ children, disabled, to, type }) {
         return (
             <button
                 disabled={disabled}
+                onClick={onClick}
                 className={styles[type]}>
                 {children}
             </button>
