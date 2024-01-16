b2bb09d9f5ceb65732b58b83bdaf00c33fded87f
Lauv <pussaaon@gmail.com>
2024-01-16 10:27:54 +0700

    project wild-oasis: create and style components

--- hands-on/part4/the-wild-oasis/src/App.jsx
+++ hands-on/part4/the-wild-oasis/src/App.jsx
@@ -1,11 +1,18 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 function App() {
     return (
         <>
             <GlobalStyles />
-            <Headline>Wild Oasis</Headline>
+            <div>
+                <Headline>Wild Oasis</Headline>
+                <Button>Check in</Button>
+                <Button>Check out</Button>
+                <Input placeholder="Search" />
+            </div>
         </>
     );
 }
