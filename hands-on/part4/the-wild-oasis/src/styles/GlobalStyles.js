123b20add05ebc226c6e82b47dd66e77b7a89448
Lauv <pussaaon@gmail.com>
2024-01-16 11:34:17 +0700

    project wild-oasis: organize global style indents

--- hands-on/part4/the-wild-oasis/src/styles/GlobalStyles.js
+++ hands-on/part4/the-wild-oasis/src/styles/GlobalStyles.js
@@ -54,89 +54,89 @@ const GlobalStyles = createGlobalStyle`
 	/* For dark mode */
 	--image-grayscale: 0;
 	--image-opacity: 100%;
-	}
+}
 
-	*,
-	*::before,
-	*::after {
+*,
+*::before,
+*::after {
 	box-sizing: border-box;
 	padding: 0;
-	margin: 0;
+	margin: 0;	
 
 	/* Creating animations for dark mode */
 	transition: background-color 0.3s, border 0.3s;
-	}
+}
 
-	html {
+html {
 	font-size: 62.5%;
-	}
+}
 
-	body {
+body {
 	font-family: "Poppins", sans-serif;
 	color: var(--color-grey-700);
 
 	transition: color 0.3s, background-color 0.3s;
 	min-height: 100vh;
 	line-height: 1.5;
-	font-size: 1.6rem;
-	}
+	font-size: 1.6rem;	
+}
 
-	input,
-	button,
-	textarea,
-	select {
+input,
+button,
+textarea,
+select {
 	font: inherit;
 	color: inherit;
-	}
+}
 
-	button {
+button {
 	cursor: pointer;
-	}
+}
 
-	*:disabled {
+*:disabled {
 	cursor: not-allowed;
-	}
+}
 
-	select:disabled,
-	input:disabled {
+select:disabled,
+input:disabled {
 	background-color: var(--color-grey-200);
 	color: var(--color-grey-500);
-	}
+}
 
-	input:focus,
-	button:focus,
-	textarea:focus,
-	select:focus {
+input:focus,
+button:focus,
+textarea:focus,
+select:focus {
 	outline: 2px solid var(--color-brand-600);
 	outline-offset: -1px;
-	}
+}
 
-	/* Parent selector, finally 😃 */
-	button:has(svg) {
+/* Parent selector, finally 😃 */
+button:has(svg) {
 	line-height: 0;
-	}
+}
 
-	a {
+a {
 	color: inherit;
 	text-decoration: none;
-	}
+}
 
-	ul {
+ul {
 	list-style: none;
-	}
+}
 
-	p,
-	h1,
-	h2,
-	h3,
-	h4,
-	h5,
-	h6 {
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
 	overflow-wrap: break-word;
 	hyphens: auto;
-	}
+}
 
-	img {
+img {
 	max-width: 100%;
 
 	/* For dark mode */
