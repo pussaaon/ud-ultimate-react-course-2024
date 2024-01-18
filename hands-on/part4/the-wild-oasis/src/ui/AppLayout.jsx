8bf6e85de5f2a87eda9f03e9033f24fce264ad5c
Lauv <angelalauvis@gmail.com>
2024-01-18 15:55:18 +0700

    project wild-oasis: fix sidebar position

--- hands-on/part4/the-wild-oasis/src/ui/AppLayout.jsx
+++ hands-on/part4/the-wild-oasis/src/ui/AppLayout.jsx
@@ -14,6 +14,7 @@ const Main = styled.main`
     background-color: var(--color-grey-0);
     padding: 4rem 4.8rem 6.4rem;
     height: 100%;
+    overflow: scroll;
 `;
 
 const Container = styled.div`
