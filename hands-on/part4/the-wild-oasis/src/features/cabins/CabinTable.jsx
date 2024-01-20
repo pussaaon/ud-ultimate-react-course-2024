c00eac9ee3924ee514b674eea318f19bbde1e953
Lauv <angelalauvis@gmail.com>
2024-01-20 09:29:37 +0700

    project wild-oasis: handle filter cabins by discount on the client side

--- hands-on/part4/the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ hands-on/part4/the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -2,11 +2,28 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import useCabins from "./hooks/useCabins";
 import Table from "../../ui/Table";
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
     const { isLoading, cabins } = useCabins();
+    const [searchParams] = useSearchParams();
 
     if (isLoading) return <Spinner />;
+
+    const filterValue = searchParams.get("discount") || "all";
+
+    let filterCabins;
+    switch (filterValue) {
+        case "all":
+            filterCabins = cabins;
+            break;
+        case "no-discount":
+            filterCabins = cabins.filter((cabin) => cabin.discount === 0);
+            break;
+        case "with-discount":
+            filterCabins = cabins.filter((cabin) => cabin.discount > 0);
+            break;
+    }
 
     return (
         <Table columnsWidth="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -19,7 +36,7 @@ function CabinTable() {
                 <div></div>
             </Table.Header>
             <Table.Body
-                data={cabins}
+                data={filterCabins}
                 render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
             />
         </Table>
---
+++ part4-sec29-complete-advanced-web-project.md
@@ -0,0 +1,13 @@
+# Packages Install
+
+```bash
+
+```
+
+# Section Topics
+
+-   Building real-world app features
+-   Filter, sort, pagination
+-   Dark mode
+-   Dashboard with charts
+-   Authentication and authorization
