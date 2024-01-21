882edbf45aeb1ec1d5169d99776e24df29877657
Lauv <angelalauvis@gmail.com>
2024-01-21 09:23:42 +0700

    project wild-oasis: apply server side filtering for bookings' status

--- hands-on/part4/the-wild-oasis/src/features/bookings/hooks/useBookings.js
+++ hands-on/part4/the-wild-oasis/src/features/bookings/hooks/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 function useBookings() {
+    const [searchParams] = useSearchParams();
+    const filterValue = searchParams.get("status");
+    const filter =
+        !filterValue || filterValue === "all"
+            ? null
+            : { field: "status", value: filterValue };
+
     const {
         isLoading,
         error,
         data: bookings,
     } = useQuery({
-        queryFn: getBookings,
-        queryKey: ["bookings"],
+        queryFn: () => getBookings(filter),
+        queryKey: ["bookings", filter],
     });
 
     return { isLoading, error, bookings };
