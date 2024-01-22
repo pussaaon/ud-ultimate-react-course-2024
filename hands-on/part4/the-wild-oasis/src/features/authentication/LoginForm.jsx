ef7576f127ca780e5985456f6866341c7a5c1e71
Lauv <angelalauvis@gmail.com>
2024-01-22 22:06:02 +0700

    project wild-oasis: clear email and password after login called

--- hands-on/part4/the-wild-oasis/src/features/authentication/LoginForm.jsx
+++ hands-on/part4/the-wild-oasis/src/features/authentication/LoginForm.jsx
@@ -15,7 +15,15 @@ function LoginForm() {
 
         if (!email || !password) return;
 
-        login({ email, password });
+        login(
+            { email, password },
+            {
+                onSettled: () => {
+                    setEmail("");
+                    setPassword("");
+                },
+            }
+        );
     }
 
     return (
